8cad6e07c2b0aa59aaae0709da296224eb30996b
Lutfi Ardiansyah <lutfi@kct.co.id>
2022-07-19 17:48:24 +0700

    Update from stat -> statSync and remove unused log

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const path = require("path");
 
-const PATH_SOURCE = "/downloads";
-const PATH_DESTINATION = "/tmp";
+const PATH_SOURCE = "C:\\Users\\Lutfi Ardiansyah\\Downloads\\Documents";
+const PATH_DESTINATION = "C:\\Users\\Lutfi Ardiansyah\\Downloads\\Documents";
 const REMOVE_OLD = false;
 
 async function main() {
@@ -19,10 +19,8 @@ async function main() {
   for (let index = 1; index <= countFiles; index++) {
     const file = files[index - 1];
 
-    console.log(file);
-
     let fileName = file.name;
-    fs.stat(`${PATH_SOURCE}${path.sep}${fileName}`, function (err, stats) {
+    fs.statSync(`${PATH_SOURCE}${path.sep}${fileName}`, function (err, stats) {
       if (!err) {
         let year = stats.mtime.getFullYear();
         let destination = `${PATH_DESTINATION}${path.sep}${year}${path.sep}`;
